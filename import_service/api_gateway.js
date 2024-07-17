553b68f6e26d021e034344b1e5e6a6f45021c4ad
marucat <belolutskaya@gmail.com>
2024-07-17 23:53:10 +0300

    fix 2

--- import_service/api_gateway.js
+++ import_service/api_gateway.js
@@ -21,12 +21,14 @@ class ApiGateway extends Stack {
     
     const api = new aws_apigateway.RestApi(this, 'ImportApi', {
         restApiName: 'Import Service',
-        // defaultCorsPreflightOptions: {
-        //   allowOrigins: aws_apigateway.Cors.ALL_ORIGINS,
-        //   allowMethods: aws_apigateway.Cors.ALL_METHODS,
-        //   allowHeaders: aws_apigateway.Cors.DEFAULT_HEADERS,
-        //   allowCredentials: true,
-        // },
+        cloudWatchRole: true,
+        description: "This service import products for RSS AWS course.",
+        defaultCorsPreflightOptions: {
+          allowOrigins: aws_apigateway.Cors.ALL_ORIGINS,
+          allowMethods: aws_apigateway.Cors.ALL_METHODS,
+          allowHeaders: aws_apigateway.Cors.DEFAULT_HEADERS,
+          allowCredentials: true,
+        },
     });
 
     const basicAuthorizerArn = Fn.importValue("BasicAuthorizerArn");
@@ -83,38 +85,38 @@ class ApiGateway extends Stack {
       statusCode: "403",
     });
 
-    import_resource.addMethod('OPTIONS', new aws_apigateway.MockIntegration({
-      integrationResponses: [
-        {
-          statusCode: '200',
-          responseParameters: {
-            'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-            'method.response.header.Access-Control-Allow-Origin': "'*'",
-            'method.response.header.Access-Control-Allow-Methods': "'GET,POST,OPTIONS'",
-            'method.response.header.Access-Control-Max-Age': "'1728000'",
-          },
-          responseTemplates: {
-            'application/json': '{"statusCode": 200}'
-          },
-        },
-      ],
-      passthroughBehavior: aws_apigateway.PassthroughBehavior.NEVER,
-      requestTemplates: {
-        'application/json': '{"statusCode": 200}'
-      },
-    }), {
-      methodResponses: [
-        {
-          statusCode: '200',
-          responseParameters: {
-            'method.response.header.Access-Control-Allow-Headers': true,
-            'method.response.header.Access-Control-Allow-Methods': true,
-            'method.response.header.Access-Control-Allow-Origin': true,
-            'method.response.header.Access-Control-Max-Age': true,
-          },
-        },
-      ],
-    });
+    // import_resource.addMethod('OPTIONS', new aws_apigateway.MockIntegration({
+    //   integrationResponses: [
+    //     {
+    //       statusCode: '200',
+    //       responseParameters: {
+    //         'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    //         'method.response.header.Access-Control-Allow-Origin': "'*'",
+    //         'method.response.header.Access-Control-Allow-Methods': "'GET,POST,OPTIONS'",
+    //         'method.response.header.Access-Control-Max-Age': "'1728000'",
+    //       },
+    //       responseTemplates: {
+    //         'application/json': '{"statusCode": 200}'
+    //       },
+    //     },
+    //   ],
+    //   passthroughBehavior: aws_apigateway.PassthroughBehavior.NEVER,
+    //   requestTemplates: {
+    //     'application/json': '{"statusCode": 200}'
+    //   },
+    // }), {
+    //   methodResponses: [
+    //     {
+    //       statusCode: '200',
+    //       responseParameters: {
+    //         'method.response.header.Access-Control-Allow-Headers': true,
+    //         'method.response.header.Access-Control-Allow-Methods': true,
+    //         'method.response.header.Access-Control-Allow-Origin': true,
+    //         'method.response.header.Access-Control-Max-Age': true,
+    //       },
+    //     },
+    //   ],
+    // });
 
   }
 }
